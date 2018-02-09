fa0b57e229e5b209c08bb6838ab50336a5998c16
Andrea Vibelli <andrea.vibelli@gmail.com>
2018-02-09 14:41:54 +0100

    Fixed model mapper and fetch adjustements

--- src/main/java/org/jboss/license/dictionary/utils/Mappers.java
+++ src/main/java/org/jboss/license/dictionary/utils/Mappers.java
@@ -1,7 +1,8 @@
 package org.jboss.license.dictionary.utils;
 
-import api.License;
-import org.jboss.license.dictionary.LicenseEntity;
+import api.LicenseRest;
+
+import org.jboss.license.dictionary.model.License;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 
@@ -16,10 +17,10 @@ import java.util.List;
  */
 public class Mappers {
 
-    public static final Type licenseListType = new TypeToken<List<License>>() {
+    public static final Type licenseRestListType = new TypeToken<List<LicenseRest>>() {
     }.getType();
 
-    public static final Type licenseEntityListType = new TypeToken<List<LicenseEntity>>() {
+    public static final Type licenseListType = new TypeToken<List<License>>() {
     }.getType();
 
     public static final ModelMapper fullMapper;
@@ -27,9 +28,9 @@ public class Mappers {
 
     static {
         limitedMapper = new ModelMapper();
-        limitedMapper.typeMap(LicenseEntity.class, License.class).addMappings(mapping -> {
-            mapping.skip(License::setContent);
-        });
+        // limitedMapper.typeMap(License.class, LicenseRest.class).addMappings(mapping -> {
+        // mapping.skip(License::setContent);
+        // });
 
         fullMapper = new ModelMapper();
     }
