3d66e94066eb94f8f14b1aa1554ac32d8df130b6
Andrea Vibelli <andrea.vibelli@gmail.com>
2018-02-27 09:47:07 +0100

    Fix of failing tests and update to new model and structure

--- src/main/java/org/jboss/license/dictionary/LicenseDbStore.java
+++ src/main/java/org/jboss/license/dictionary/LicenseDbStore.java
@@ -32,6 +32,12 @@ public class LicenseDbStore {
         return license;
     }
 
+    public License update(License license) {
+        log.debug("Updating license: " + license);
+        license = entityManager.merge(license);
+        return license;
+    }
+
     public License getLicense(Integer id) {
         log.debugf("Get license by id:  %d", id);
         return entityManager.find(License.class, id);
@@ -77,5 +83,11 @@ public class LicenseDbStore {
                 log.error(exc);
             }
         });
+    }
+
+    public LicenseApprovalStatus saveLicenseApprovalStatus(LicenseApprovalStatus licenseApprovalStatus) {
+        log.debug("Saving licenseApprovalStatus: " + licenseApprovalStatus);
+        entityManager.persist(licenseApprovalStatus);
+        return licenseApprovalStatus;
     }
 }
